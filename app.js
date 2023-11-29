053d2d47841b4c16eb0ee4f148e69f6ab234b64d
LeidyRom <142278329+LeidyRom@users.noreply.github.com>
2023-11-28 22:26:29 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,100 +1,132 @@
-//Eventos para crear nuevos productos
+// Evento para crear nuevos productos
 document.getElementById("Lista de productos").addEventListener("submit", crear);
 
-//Función crear
+// Función crear
+function crear(e) {
+    e.preventDefault();
 
-function crear(e){
-    titulo= document.getElementById("producto"),value
-    descripción= document.getElementById("descrpción"),value
-    precio = document.getElementById("precio"),value
-}
-    
+    let titulo = document.getElementById("producto").value;
+    let descripcion = document.getElementById("descripcion").value;
+    let vencimiento = document.getElementById("fecha de vencimiento").value;
+    let lote = document.getElementById("lote").value;
+    let precio = document.getElementById("precio").value;
+
     let producto = {
-        productos,
-        Descripción,
-        Precio,
+        producto: titulo,
+        descripcion: descripcion,
+        vencimiento: vencimiento,
+        lote: lote,
+        precio: precio
+    };
+
+    if (localStorage.getItem("productos") === null) {
+        let productos = [];
+        productos.push(producto);
+        localStorage.setItem("productos", JSON.stringify(productos));
+    } else {
+        let productos = JSON.parse(localStorage.getItem("productos"));
+        productos.push(producto);
+        localStorage.setItem("productos", JSON.stringify(productos));
     }
 
-    if(localStorage = ("productos"),seenull){
-        let productos = []
-        productos.push(productos)
-        localStorage.setItem("productos",JSON.stringify(productos))
-    }
-    else;{ 
-        let productos = JSON.parse(localStorage.getItem("productos"))
-        productos.push(producto)
-        localStorage.setItem("productos", JSON.stringify(productos))
-    }
-    
-    leer();{
-        document.getElementById("Lista de productos").reset();
-        console.log("producto agregado")
-        e.preventDefault()
-    }
-    
-//función leer
+    leer();
+}
 
-function leer(){
+// Función leer
+function leer() {
+    let productos = JSON.parse(localStorage.getItem("productos")) || [];
+    let tbody = document.getElementById("tbody");
+    tbody.innerHTML = "";
 
-    let productos =JSON.parse(localStorage.getItem("productos"));
-    document.getElementById("tbody").innerHTML = ""
-    for(let i=0; i < productos.length; i++){
+    for (let i = 0; i < productos.length; i++) {
         let producto = productos[i].producto;
-        let descripción = productos[i].descripción;
+        let descripcion = productos[i].descripcion;
+        let fechaVencimiento = productos[i].vencimiento;
+        let lote = productos[i].lote;
         let precio = productos[i].precio;
 
-    document.getElementById("tbody").innerHTML +=
+        let fila = `
+            <tr>
+                <td>${producto}</td>
+                <td>${descripcion}</td>
+                <td>${fechaVencimiento}</td>
+                <td>${lote}</td>
+                <td>${precio}</td>
+                <td>
+                    <button onclick="eliminar('${producto}')">Eliminar</button>
+                    <button onclick="editar('${producto}')">Editar</button>
+                </td>
+            </tr>
+        `;
+        tbody.innerHTML += fila;
+    }
 
-        <>'
-        <tr>
-            <td>$(Productos)</td>
-            <td>$(descripción)</td>
-            <td>$(Precio)</td>
-            <td><button onclick="Eliminar('${título}')" class="btn btn-danger">Eliminar</button></td>
-        </tr><td><button onclick="Editar('${título}')" class="btn btn-success">Editar</button></td></>
+    // Limpiar el formulario
+    document.getElementById("Lista de productos").reset();
+    console.log("Producto agregado");
+}
 
-        '
+// Función eliminar
+function eliminar(producto) {
+    let productos = JSON.parse(localStorage.getItem("productos"));
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].producto === producto) {
+            productos.splice(i, 1);
+        }
+    }
+    localStorage.setItem("productos", JSON.stringify(productos));
+    leer();
 }
-   
-}
-//función editar
-        
-function editar(título){
-    let Productos = JSON.parse(localStorage.getItem("productos"));
-    for(let i=0; i < productos,length; i++){
-        if(productos[i].productos === productos){
-            document.getElementById("body").innerHTML =
-            '<div class="row">
-            <div class="col-md-5">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Productos</h2>
-                    </div>
-                    <div class="card-body">
-                        <form>
-                            <div class="form-group">
-                                <input type="text" id="newproducto" class="form-control my-3" placeholder="Ingresar producto">
-                            </div>
-                            <div class="form-group">
-                                <textarea id="newdescripcion" class="form-control my-3" placeholder="Descripción del producto"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <input type="number" id="newprecio" class="form-control my-3" placeholder="Precio">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Agregar</button>
-                        </form>
-                        <button class "btn btn- success" onclick ="Actualizar('$[i]')">Actualizar</button>
-                        <button class "btn btn- primary" onclick ="Actualizar('$[i]')">Cancelar</button>
-    
-    
-                    </div>
+
+// Función editar
+function editar(titulo) {
+    let productos = JSON.parse(localStorage.getItem("productos"));
+
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].producto === titulo) {
+            document.getElementById("formulario-edicion").innerHTML =
+                `
+                <!-- Formulario de edición -->
+                <div class="form-group">
+                    <input type="text" id="newproducto" class="form-control my-3" placeholder='${productos[i].producto}'>
                 </div>
-            </div>
-        
-        
-        '}
-        
+                <div class="form-group">
+                    <textarea id="newdescripcion" class="form-control my-3" placeholder='${productos[i].descripcion}'></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="text" id="newvencimiento" class="form-control my-3" placeholder='${productos[i].vencimiento}'/>
+                </div>
+                <div class="form-group">
+                    <input type="text" id="newlote" class="form-control my-3" placeholder='${productos[i].lote}'/>
+                </div>
+                <div class="form-group">
+                    <input type="number" id="newprecio" class="form-control my-3" placeholder='${productos[i].precio}'/>
+                </div>
+                <button class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
+                <button class="btn btn-primary" onclick="cancelar()">Cancelar</button>
+                `;
+        }
     }
 }
 
-leer();
+// Función actualizar
+function actualizar(i) {
+    let productos = JSON.parse(localStorage.getItem("productos"));
+    productos[i].producto = document.getElementById("newproducto").value;
+    productos[i].descripcion = document.getElementById("newdescripcion").value;
+    productos[i].vencimiento = document.getElementById("newvencimiento").value;
+    productos[i].lote = document.getElementById("newlote").value;
+    productos[i].precio = document.getElementById("newprecio").value;
+
+    localStorage.setItem("productos", JSON.stringify(productos));
+    leer();
+
+    // Limpiar el formulario de edición
+    document.getElementById("formulario-edicion").innerHTML = "";
+}
+
+// Función cancelar
+function cancelar() {
+    // Limpiar el formulario de edición
+    document.getElementById("formulario-edicion").innerHTML = "";
+}
